38c41e5aded8dd8983a2b906acc1efe4c52545a3
Mesut Yılmaz <im.mesut.yilmaz@gmail.com>
2023-02-07 13:35:38 +0300

    update

--- parser.js
+++ parser.js
@@ -101,6 +101,13 @@ getPage().then((parsed) => {
 
   let lastHalf = `
 </body>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-52JK4HL0XW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'G-52JK4HL0XW');
+</script>
 </html>
 `
 
